b9734bae57f96af2908a1a0f30f72b6b618c9dc9
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-08 11:25:51 +0530

    404

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Dev from '../components/Dev'
+
+const About  = () => {
+  return (
+    <Dev></Dev>
+  )
+}
+
+export default Dev
+
---
+++ pages/products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Dev from '../components/Dev'
+const products = () => {
+  return (
+    <div><Dev></Dev></div>
+  )
+}
+
+export default products
