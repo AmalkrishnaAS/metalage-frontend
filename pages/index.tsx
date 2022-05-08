13e238b25a940c7b8c4a05559b4635584f76ab2a
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-08 11:48:54 +0530

    static files

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,10 @@ const Home: NextPage = () => {
      <div><Navbar></Navbar></div>
       <Hero></Hero>
       <Features></Features>
-      <Slider heading='Food processing' desc='We offer a comprehensive line of technically advanced food processing and packaging machines.' img='http://www.metalagemachineries.com/images/feature-img.jpg'></Slider>
-      <Slider heading='Rice Powder Units' desc='Our engineers work in tandem with the clients to understand their requirements and provide feasible solutions for the specific process application.' img='http://www.metalagemachineries.com/images/rice.jpg' ></Slider>
-      <Slider heading='Ayurvedic' desc='We are one of the leading supplier of a variety of Ayurvedic processing Machines and related Equipmenst required for Ayurvedic production' img='http://www.metalagemachineries.com/images/ayur.jpg'></Slider>
-      <Slider heading='Hotels & Catering' desc='We have adopted latest technologies available worldwide and collaborated with key international players.our plants ensure consistency in production of high quality products.' img='http://www.metalagemachineries.com/images/cat.jpg'></Slider>
+      <Slider heading='Food processing' desc='We offer a comprehensive line of technically advanced food processing and packaging machines.' img='/food.jpg'></Slider>
+      <Slider heading='Rice Powder Units' desc='Our engineers work in tandem with the clients to understand their requirements and provide feasible solutions for the specific process application.' img='/rice.jpg' ></Slider>
+      <Slider heading='Ayurvedic' desc='We are one of the leading supplier of a variety of Ayurvedic processing Machines and related Equipmenst required for Ayurvedic production' img='ayurvedic.jpg'></Slider>
+      <Slider heading='Hotels & Catering' desc='We have adopted latest technologies available worldwide and collaborated with key international players.our plants ensure consistency in production of high quality products.' img='/hotel.jpg'></Slider>
       <Footer></Footer>
       
       
