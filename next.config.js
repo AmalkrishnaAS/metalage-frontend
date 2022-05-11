278254edf01417cc585c04e48465078c23206a2c
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-12 03:42:35 +0530

    animate

--- next.config.js
+++ next.config.js
@@ -4,5 +4,10 @@ module.exports = {
   images:{
     domains: ['localhost:3000','metalage.net']
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   
 }
