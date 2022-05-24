512b3d4c235ddd9ddf3f290018fdee39e36b4f40
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-05-24 22:13:01 +0530

    slider final

--- components/CTA_slider.jsx
+++ components/CTA_slider.jsx
@@ -53,7 +53,7 @@ export default function App() {
         }
         
         cssMode={true}
-        navigation={true}
+        
         pagination={true}
         mousewheel={true}
         keyboard={true}
